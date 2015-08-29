cd6fe1d66f76d3e018edc01f961260ddab7dc273
T L <tslimkemann42@gmail.com>
2015-08-29 01:03:01 -0700

    Fixed some typo bugs

--- Element.cpp
+++ Element.cpp
@@ -8,7 +8,7 @@ Element::Element(float xPos, float yPos, types t) {
     type = t;
 }
 
-Element::Tick(std::vector<Element> otherPixels) {
+void Element::tick(std::vector<Element> otherPixels) {
     for (auto e : world.pixels) { //for every pixel
         if (e != this) { //that isn't the current one
             if ((e.x - this.x < 1.01) || (this.x - HEIGHT < 5)) { //check if there's another pixel below or you're on the ground
--- Element.h
+++ Element.h
@@ -1,8 +1,11 @@
 #pragma once
 #include "ElementTypes.h"
+#include <vector>
+
 class Element {
 public:
     Element(float xPos, float yPos, types t);
+    void tick(std::vector<Element> otherPixels);
 	double x;
 	double y;
 	double xVel;
