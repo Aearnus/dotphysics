68f26b47a97fc6034da505ce975cb243e09275a5
T L <tslimkemann42@gmail.com>
2015-08-29 01:05:13 -0700

    Fixed dumb pointer bugs

--- Element.cpp
+++ Element.cpp
@@ -9,9 +9,9 @@ Element::Element(float xPos, float yPos, types t) {
 }
 
 void Element::tick(std::vector<Element> otherPixels) {
-    for (auto e : world.pixels) { //for every pixel
-        if (e != this) { //that isn't the current one
-            if ((e.x - this.x < 1.01) || (this.x - HEIGHT < 5)) { //check if there's another pixel below or you're on the ground
+    for (auto e : otherPixels) { //for every pixel
+        if (&e != this) { //that isn't the current one
+            if ((e.x - this->x < 1.01) || (this->x - HEIGHT < 5)) { //check if there's another pixel below or you're on the ground
                 xVel = 0;
             } else {
                 xVel += GRAVITY;
