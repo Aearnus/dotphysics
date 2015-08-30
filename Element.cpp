1bd11506f062b87a1a1d4d5343902f7a9c887681
T L <tslimkemann42@gmail.com>
2015-08-29 17:01:35 -0700

    Fixed Element::tick() more

--- Element.cpp
+++ Element.cpp
@@ -12,9 +12,10 @@ Element::Element(float xPos, float yPos, types t) {
 
 void Element::tick(Element* otherPixels[]) {
     int willApplyGravity = 1;
-    for (auto e : otherPixels) { //for every pixel
-        if (&e != this) { //that isn't the current one
-            if ((y - e.y < 1.01) || (y > 5) || floor(x) - floor(e.x) == 0){ //check if there's another pixel below or you're on the ground
+    for (int pixelIndex = 0; pixelIndex < WORLD_SIZE; ++pixelIndex) { //for every pixel
+        Element* currentPixel = otherPixels[pixelIndex];
+        if (currentPixel != this) { //that isn't the current one
+            if ((y - currentPixel->y < 1.01) || (y > 5) || floor(x) - floor(currentPixel->x) == 0){ //check if there's another pixel below or you're on the ground
                 willApplyGravity = 0;
             }
         }
